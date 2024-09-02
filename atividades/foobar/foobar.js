d34d2412247cb25925ea83d6101817c82570a4c1
stroligo <gabrielstroligo@gmail.com>
2024-09-03 00:34:12 +0300

    estudos

--- atividades/foobar/foobar.js
+++ atividades/foobar/foobar.js
@@ -2,18 +2,14 @@
 // Valor divisivel por 5 bar
 // Valor divisivel por 3 e 5 foobar
 
-for (let i = 0; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log('foobar');
+  } else if (i % 3 === 0) {
+    console.log('foo');
+  } else if (i % 5 === 0) {
+    console.log('bar');
   } else {
-    if (i % 3 == 0) {
-      console.log('foo');
-    } else {
-      if (i % 5 == 0) {
-        console.log('bar');
-      } else {
-        console.log(i);
-      }
-    }
+    console.log(i);
   }
 }
