a61d979ce28cf21ccd31cf3640608717398e3592
stroligo <gabrielstroligo@gmail.com>
2024-09-02 23:12:23 +0300

    estudos

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+// Declaração de uma variável 'texto'.
+const texto = 'Gabriel Stroligo';
+// Declaração de uma variável 'numeros'.
+const n1 = 10;
+const n2 = 12;
+// Imprime o valor de 'texto' no console.
+console.log(n1 + n2);
---
+++ src/ts/main.ts
@@ -0,0 +1,9 @@
+// Declaração de uma variável 'texto'.
+const texto: string = 'Gabriel Stroligo';
+
+// Declaração de uma variável 'numeros'.
+const n1: number = 10;
+const n2: number = 12;
+
+// Imprime o valor de 'texto' no console.
+console.log(n1 + n2);
